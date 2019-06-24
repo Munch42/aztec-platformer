395a9e6a4bd60a901efed838d95d92d7b7fb09a3
Cyriel De Neve <cyriel@shaw.ca>
2019-06-23 22:36:24 -0600

    Added basic horizontal player movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private Rigidbody2D _rb2d;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+
+        _rb2d.velocity = new Vector2(moveInput * speed * Time.fixedDeltaTime, _rb2d.velocity.y);
+    }
+}
