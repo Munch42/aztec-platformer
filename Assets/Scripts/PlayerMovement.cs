5dedf1cafa7fb79e589dc763388332d42532d258
Munch42 <Munch4242@gmail.com>
2019-06-25 16:23:53 -0600

    Further Improved Jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -34,6 +34,10 @@ public class PlayerMovement : MonoBehaviour
         {
             _rb2d.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
         }
+        else if (_rb2d.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            _rb2d.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
 
         /*if (Input.GetKeyDown(KeyCode.Space))
         { 
