c073fea4c6bb1bcaf9fe6690823f98f287a95703
Munch42 <Munch4242@gmail.com>
2019-06-25 08:23:20 -0600

    Improved Jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,12 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float speed;
+    [Range(1,50)]
     public float jumpForce;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
     private float moveInput;
+    private bool jumping;
 
     private Rigidbody2D _rb2d;
 
@@ -19,11 +23,26 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void Update()
-    {
+    { 
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            _rb2d.velocity = new Vector2(_rb2d.velocity.x, jumpForce * 2 * Time.deltaTime);
+            _rb2d.velocity = Vector2.up * jumpForce; //new Vector2(_rb2d.velocity.x, jumpForce * 2 * Time.deltaTime);
         }
+
+        if (_rb2d.velocity.y < 0)
+        {
+            _rb2d.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+
+        /*if (Input.GetKeyDown(KeyCode.Space))
+        { 
+            jumping = true;
+        }
+        else
+        {
+            jumping = false;
+        }*/
     }
 
     // Update is called once per frame
@@ -32,5 +51,10 @@ public class PlayerMovement : MonoBehaviour
         moveInput = Input.GetAxis("Horizontal");
 
         _rb2d.velocity = new Vector2(moveInput * speed * Time.fixedDeltaTime, _rb2d.velocity.y);
+
+        /*if (jumping == true)
+        {
+            _rb2d.velocity = new Vector2(_rb2d.velocity.x, jumpForce * 2 * Time.fixedDeltaTime);
+        }*/
     }
 }
