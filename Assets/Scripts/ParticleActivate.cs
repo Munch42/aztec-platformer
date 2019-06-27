3cade6e0a9f772b38dded29085f2214ce1feecd0
Munch42 <Munch4242@gmail.com>
2019-06-26 19:04:55 -0600

    Added Smoke Effects when you touch Sacrifice

---
+++ Assets/Scripts/ParticleActivate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleActivate : MonoBehaviour
+{
+
+    public ParticleSystem smokeParticleSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        smokeParticleSystem.Stop();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {   
+        smokeParticleSystem.Play();
+        Debug.Log("You won!");  
+    }
+}
